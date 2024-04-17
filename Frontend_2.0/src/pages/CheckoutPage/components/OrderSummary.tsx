d6037135f104cb7274783be10c8cdc747c5ba625
Freyja Egelund Grønnemose <113907244+fuckbrugernavne@users.noreply.github.com>
2024-04-17 20:46:25 +0200

    order summary bug

--- Frontend_2.0/src/pages/CheckoutPage/components/OrderSummary.tsx
+++ Frontend_2.0/src/pages/CheckoutPage/components/OrderSummary.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { useCheckout } from "../../../hooks/useCheckout";
+import { useBasket } from "../../../hooks/useBasket";
 
 const OrderSummary: React.FC = () => {
-    const { basket, purchaseTotal } = useCheckout();
+    const { basket } = useBasket();
+    const { purchaseTotal } = useCheckout();
 
     return (
         <div>
             <h1>Order Summary</h1>
             <ul>
                 {basket.map((item) => (
-                    <li key={item.product.id}>
+                    <li key={item.product.string_id}>
                         <span>{item.product.name}</span>
                         <span>{item.product.price}</span>
                     </li>
