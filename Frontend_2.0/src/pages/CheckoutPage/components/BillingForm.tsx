310555995dc077ed064ecab77de57db0e6f8808f
Bayan <113907779+BayanDowairi@users.noreply.github.com>
2024-04-24 11:09:42 +0200

    css to billin info added

--- Frontend_2.0/src/pages/CheckoutPage/components/BillingForm.tsx
+++ Frontend_2.0/src/pages/CheckoutPage/components/BillingForm.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useCheckout } from "../../../hooks/useCheckout";
 import "../../BasketPage/BasketPage.css";
 import { SpinningCircles } from "react-loading-icons";
-import "./BillingForm.css";
 
 interface Errors {
   firstNameError?: string;
@@ -186,96 +185,115 @@ const BillingForm: React.FC = () => {
   console.log("Errors", errors);
 
   return (
-    <div>
+    <div className="form">
       <div>
         <label>First Name</label>
-        <input
-          type="text"
-          name="firstName"
-          required
-          value={billingInfo.firstName}
-          onChange={handleChange}
-        />
+        <div className="fields">
+          <input
+            className="input_field"
+            type="text"
+            name="firstName"
+            required
+            value={billingInfo.firstName}
+            onChange={handleChange}
+          />
+        </div>
       </div>
       <div>
         <label>Last Name</label>
-        <input
-          type="text"
-          name="lastName"
-          required
-          value={billingInfo.lastName}
-          onChange={handleChange}
-        />
+        <div className="fields">
+          <input
+            type="text"
+            name="lastName"
+            required
+            value={billingInfo.lastName}
+            onChange={handleChange}
+          />
+        </div>
       </div>
       <div>
         <label>Email</label>
-        <input
-          type="email"
-          name="email"
-          required
-          value={billingInfo.email}
-          onChange={handleChange}
-        />
+        <div className="fields">
+          <input
+            type="email"
+            name="email"
+            required
+            value={billingInfo.email}
+            onChange={handleChange}
+          />
+        </div>
       </div>
       <div>
         <label>Address Line1</label>
-        <input
-          type="text"
-          name="address1"
-          required
-          value={billingInfo.address1}
-          onChange={handleChange}
-        />
+        <div className="fields">
+          <input
+            type="text"
+            name="address1"
+            required
+            value={billingInfo.address1}
+            onChange={handleChange}
+          />
+        </div>
       </div>
       <div>
         <label>Address Line2(Optional)</label>
-        <input
-          type="text"
-          name="address2"
-          value={billingInfo.address2 || ""}
-          onChange={handleChange}
-        />
+        <div className="fields">
+          <input
+            type="text"
+            name="address2"
+            value={billingInfo.address2 || ""}
+            onChange={handleChange}
+          />
+        </div>
       </div>
       <div>
         <label>Postal Code</label>
-        <input
-          type="text"
-          name="postalCode"
-          required
-          maxLength={4}
-          value={billingInfo.postalCode}
-          onChange={handleChange}
-        />
+        <div className="fields">
+          <input
+            type="text"
+            name="postalCode"
+            required
+            maxLength={4}
+            value={billingInfo.postalCode}
+            onChange={handleChange}
+          />
+        </div>
       </div>
       <div>
         <label>City</label>
-        <input
-          type="text"
-          name="city"
-          value={billingInfo.city}
-          onChange={handleChange}
-        />
+        <div className="fields">
+          <input
+            type="text"
+            name="city"
+            value={billingInfo.city}
+            onChange={handleChange}
+          />
+        </div>
       </div>
       <div>
         <label>Phone</label>
-        <input
-          type="text"
-          name="phone"
-          required
-          value={billingInfo.phone}
-          onChange={handleChange}
-        />
+        <div className="fields">
+          <input
+            type="text"
+            name="phone"
+            required
+            value={billingInfo.phone}
+            onChange={handleChange}
+          />
+        </div>
       </div>
       <div>
         <label>Country</label>
-        <select
-          name="country"
-          required
-          value={billingInfo.country}
-          onChange={handleChange}
-        >
-          <option value="Denmark">Denmark</option>
-        </select>
+        <div className="fields">
+          <select
+            name="country"
+            required
+            value={billingInfo.country}
+            onChange={handleChange}
+          >
+            <option value="Denmark">Denmark</option>
+          </select>
+        </div>
       </div>
       <div>
         <label>
@@ -290,71 +308,85 @@ const BillingForm: React.FC = () => {
           <>
             <div>
               <label>First Name</label>
-              <input
-                type="text"
-                name="deliveryFirstName"
-                required
-                value={billingInfo.deliveryFirstName || ""}
-                onChange={handleChange}
-              />
+              <div className="fields">
+                <input
+                  type="text"
+                  name="deliveryFirstName"
+                  required
+                  value={billingInfo.deliveryFirstName || ""}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
             <div>
               <label>Last Name</label>
-              <input
-                type="text"
-                name="deliveryLastName"
-                required
-                value={billingInfo.deliveryLastName || ""}
-                onChange={handleChange}
-              />
+              <div className="fields">
+                <input
+                  type="text"
+                  name="deliveryLastName"
+                  required
+                  value={billingInfo.deliveryLastName || ""}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
             <div>
               <label>Delivery Address</label>
-              <input
-                type="text"
-                name="deliveryAddress"
-                required
-                value={billingInfo.deliveryAddress || ""}
-                onChange={handleChange}
-              />
+              <div className="fields">
+                <input
+                  type="text"
+                  name="deliveryAddress"
+                  required
+                  value={billingInfo.deliveryAddress || ""}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
             <div>
               <label>Postal Code</label>
-              <input
-                type="text"
-                name="deliveryPostalCode"
-                required
-                maxLength={4}
-                value={billingInfo.deliveryPostalCode || ""}
-                onChange={handleChange}
-              />
+              <div className="fields">
+                <input
+                  type="text"
+                  name="deliveryPostalCode"
+                  required
+                  maxLength={4}
+                  value={billingInfo.deliveryPostalCode || ""}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
             <div>
               <label>City</label>
-              <input
-                type="text"
-                name="deliveryCity"
-                value={billingInfo.deliveryCity || ""}
-                onChange={handleChange}
-              />
+              <div className="fields">
+                <input
+                  type="text"
+                  name="deliveryCity"
+                  value={billingInfo.deliveryCity || ""}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
             <div>
               <label>Company Name (Optional)</label>
-              <input
-                type="text"
-                name="companyName"
-                value={billingInfo.companyName || ""}
-                onChange={handleChange}
-              />
+              <div className="fields">
+                <input
+                  type="text"
+                  name="companyName"
+                  value={billingInfo.companyName || ""}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
             <div>
               <label>Company VAT (Optional)</label>
-              <input
-                type="text"
-                name="companyVat"
-                value={billingInfo.companyVat || ""}
-                onChange={handleChange}
-              />
+              <div className="fields">
+                <input
+                  type="text"
+                  name="companyVat"
+                  value={billingInfo.companyVat || ""}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
           </>
         )}
