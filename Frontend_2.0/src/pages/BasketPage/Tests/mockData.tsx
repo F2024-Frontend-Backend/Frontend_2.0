69f07238a664a6a528a078225f8d7023865d9fc4
NegarOstad <s224283@student.dtu.dk>
2024-04-26 21:30:03 +0200

    test basket components

---
+++ Frontend_2.0/src/pages/BasketPage/Tests/mockData.tsx
@@ -0,0 +1,39 @@
+// Mock data
+export const mockItems = [
+  {
+    product: {
+      string_id: "vitamin-d-90-120",
+      name: "D-vitamin, 90ug, 120 stk",
+      price: 116,
+      currency: "DKK",
+      imageUrl:
+        "https://apopro.dk/Images/d3-vitamin-staerk-kapsler-90-%C2%B5g-kosttilskud-120-stk-214878",
+    },
+    quantity: 2,
+    sub_total: 232,
+  },
+  {
+    product: {
+      string_id: "vitamin-c-500-200",
+      name: "C-vitamin, 500mg, 200 stk",
+      price: 150,
+      currency: "DKK",
+      imageUrl:
+        "https://images.matas.dk/Assets_v3/600001-700000/636001-637000/636601-636700/636640/productlist_v1_x2.jpg",
+    },
+    quantity: 3,
+    sub_total: 450,
+  },
+  {
+    product: {
+      string_id: "vitamin-c-depot-500-250",
+      name: "C-vitamin Depot, 500mg, 200 stk",
+      price: 175,
+      currency: "DKK",
+      imageUrl:
+        "https://images.matas.dk/Assets_v3/600001-700000/631001-632000/631601-631700/631666/product_v1_x2.jpg",
+    },
+    quantity: 1,
+    sub_total: 175,
+  },
+];
