5cbbf48dc57f45971b7f7af29517fe9502e30b2c
Asbjørn <s204026@student.dtu.dk>
2024-04-29 23:22:44 +0200

    w

--- Frontend_2.0/src/pages/CheckoutPage/CheckoutPage.tsx
+++ Frontend_2.0/src/pages/CheckoutPage/CheckoutPage.tsx
@@ -1,42 +1,25 @@
 
 import './CheckoutPage.css';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import BillingForm from './CheckoutSubPages/BillingForm';
 import PaymentForm from './CheckoutSubPages/PaymentForm';
 import Confirmation from './CheckoutSubPages/ConfirmationPage';
 import OrderSummary from './CheckoutSubPages/OrderSummary';
 
+
 const CheckoutPage = () => {
-    const location = useLocation();
-    let heading;
-    switch (location.pathname) {
-        case '/checkout':
-            heading = 'Billing Information';
-            break;
-        case '/checkout/payment':
-            heading = 'Payment Information';
-            break;
-        case '/checkout/confirmation':
-            heading = 'Confirmation';
-            break;
-        default:
-            heading = '';
-    }
     return (
-        <div className="checkout-page">
-            <div className="checkout-header">
-                    <h1>{heading}</h1>
-                </div>
-            <div className='checkout-container'>
-                <div className="checkout-forms-container">
+        <div className="checkout-container">
+            <div className="checkout-summary">
+                <OrderSummary />
+            </div>
+            <div className="checkout-wrapper">
+                <div className="checkout-forms">
                     <Routes>
                         <Route path="/" element={<BillingForm />} />
                         <Route path="payment" element={<PaymentForm />} />
                         <Route path="confirmation" element={<Confirmation />} />
                     </Routes>
-                </div>
-                <div className="checkout-summary">
-                    <OrderSummary />
                 </div>
             </div>
         </div>
